608167a2d99e6521a79a6b1f9e77aee4791c59ae
Igor <igor.vguerra@hotmail.com>
2025-07-06 22:45:06 +0100

    first test created

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def create_task():
     task_id_control += 1
     tasks.append(new_task)
     print(tasks)
-    return jsonify({"message": "New Task successfully added!"})
+    return jsonify({"message": "New Task successfully added!", "id": new_task.id})
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
